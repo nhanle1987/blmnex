d1fb45645acde68c9c7cef3473805bd63409d9d2
nhanle1987 <nhan_le1987@yahoo.com>
2024-07-30 01:31:01 +0700

    .

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,36 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   basePath: '/blmnex/demo',
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // These rewrites are checked after headers/redirects
+        // and before all files including _next/public files which
+        // allows overriding page files
+        {
+          source: '/some-page',
+          destination: '/somewhere-else',
+          has: [{ type: 'query', key: 'overrideMe' }],
+        },
+      ],
+      afterFiles: [
+        // These rewrites are checked after pages/public files
+        // are checked but before dynamic routes
+        {
+          source: '/non-existent',
+          destination: '/somewhere-else',
+        },
+      ],
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: '/:path*',
+          destination: `https://my-old-site.com/:path*`,
+        },
+      ],
+    }
+  },
 };
 
 export default nextConfig;
