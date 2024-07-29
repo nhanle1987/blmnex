05c139cc4c7576c441b6f6e75010813ee0ca10fa
nhanle1987 <nhan_le1987@yahoo.com>
2024-07-29 23:57:53 +0700

    aa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: 'build',
+  reactStrictMode: true,
+  output: "export",
+};
 
 export default nextConfig;
